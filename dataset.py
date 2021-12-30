72b3c09d725470f846ef7c945844766b04e70f85
Daniele R.C <darthreca@gmail.com>
2021-12-30 11:56:13 +0100

    Updated with torch

--- dataset.py
+++ dataset.py
@@ -14,59 +14,61 @@ It will be used in data_helper to retrieve sample's name and associated label.
 
 """
 
+
 def _dataset_info(txt_labels):
-    with open(txt_labels, 'r') as f:
+    with open(txt_labels, "r") as f:
         images_list = f.readlines()
 
     file_names = []
     labels = []
     for row in images_list:
-        row = row.split(' ')
+        row = row.split(" ")
         file_names.append(row[0])
         labels.append(int(row[1]))
 
     return file_names, labels
 
-#It returns train_dataset
+
+# It returns train_dataset
 class Dataset(data.Dataset):
-    def __init__(self, names, labels, path_dataset,img_transformer=None):
+    def __init__(self, names, labels, path_dataset, img_transformer=None):
         self.data_path = path_dataset
         self.names = names
         self.labels = labels
         self._image_transformer = img_transformer
 
     def __getitem__(self, index):
-        #TODO: get item with image and image rotation
-        #get the image path
-          image_path = self.data_path + '/' + self.names[index]
-          #produce a random label between 0,1,2,3
-          img = Image.open(image_path).convert('RGB')
+        # TODO: get item with image and image rotation
+        # get the image path
+        image_path = self.data_path + "/" + self.names[index]
+        # produce a random label between 0,1,2,3
+        img = TF.resize(TF.to_tensor(Image.open(image_path).convert("RGB")), [256, 256])
 
-          index_rot = random.randrange(4)
-          #The angles of rotation are 0°, 90°, 180°, 270° that correspond to labels 0, 1, 2, 3.
+        index_rot = random.randrange(4)
+        # The angles of rotation are 0°, 90°, 180°, 270° that correspond to labels 0, 1, 2, 3.
 
-          if index_rot == 0:
-              img_rot = img
+        if index_rot == 0:
+            img_rot = img
 
-          if index_rot == 1:
-              img_rot = torch.rot90(img, k=1, dims=[1, 2]) #(img, k=1)
+        if index_rot == 1:
+            img_rot = torch.rot90(img, k=1, dims=[1, 2])  # (img, k=1)
 
-          if index_rot == 2:
-              img_rot = torch.rot90(img, k=2, dims=[1, 2])
+        if index_rot == 2:
+            img_rot = torch.rot90(img, k=2, dims=[1, 2])
 
-          if index_rot == 3:
-              img_rot = torch.rot90(img, k=3, dims=[1, 2])
-              #convert to tensor
-          img_rot = TF.to_tensor(img_rot)
-          return img, int(self.labels[index]), img_rot, index_rot
+        if index_rot == 3:
+            img_rot = torch.rot90(img, k=3, dims=[1, 2])
+            # convert to tensor
+        # img_rot = TF.to_tensor(img_rot)
+        return img, int(self.labels[index]), img_rot, index_rot
 
     def __len__(self):
         return len(self.names)
 
 
-#It returns test_dataset
+# It returns test_dataset
 class TestDataset(data.Dataset):
-    def __init__(self, names, labels, path_dataset,img_transformer=None):
+    def __init__(self, names, labels, path_dataset, img_transformer=None):
         self.data_path = path_dataset
         self.names = names
         self.labels = labels
@@ -74,29 +76,28 @@ class TestDataset(data.Dataset):
 
     def __getitem__(self, index):
 
-        #TODO: get item with image and image rotation
-          image_path = self.data_path + '/' + self.names[index]
-          img = Image.open(image_path).convert('RGB')
-          #produce a random label between 0,1,2,3
-          index_rot = np.random.randint(4)
-          #The angles of rotation are 0°, 90°, 180°, 270° that correspond to labels 0, 1, 2, 3.
+        # TODO: get item with image and image rotation
+        image_path = self.data_path + "/" + self.names[index]
+        img = Image.open(image_path).convert("RGB")
+        # produce a random label between 0,1,2,3
+        index_rot = np.random.randint(4)
+        # The angles of rotation are 0°, 90°, 180°, 270° that correspond to labels 0, 1, 2, 3.
 
-          if index_rot == 0:
-              img_rot = img
+        if index_rot == 0:
+            img_rot = img
 
-          if index_rot == 1:
-              img_rot = np.rot90(img, k=1)
+        if index_rot == 1:
+            img_rot = np.rot90(img, k=1)
 
-          if index_rot == 2:
-              img_rot = np.rot90(img, k=2)
+        if index_rot == 2:
+            img_rot = np.rot90(img, k=2)
 
-          if index_rot == 3:
-              img_rot = np.rot90(img, k=3)
+        if index_rot == 3:
+            img_rot = np.rot90(img, k=3)
 
-          img_rot = TF.to_tensor(img_rot)
-        
-          return img, int(self.labels[index]), img_rot, index_rot
+        img_rot = TF.to_tensor(img_rot)
+
+        return img, int(self.labels[index]), img_rot, index_rot
 
     def __len__(self):
         return len(self.names)
-
--- main.py
+++ main.py
@@ -23,40 +23,98 @@ def get_args():
     The method for parsing command line arguments. Use '-h' as the command line argument to display the list of options and their usage.
 
     """
-    parser = argparse.ArgumentParser(description="Script to launch training",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description="Script to launch training",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument("--source", default='Art', help="Source name")
-    parser.add_argument("--target", default='Clipart', help="Target name")
-    parser.add_argument("--n_classes_known", type=int, default=45, help="Number of known classes")
-    parser.add_argument("--n_classes_tot", type=int, default=65, help="Number of unknown classes")
+    parser.add_argument("--source", default="Art", help="Source name")
+    parser.add_argument("--target", default="Clipart", help="Target name")
+    parser.add_argument(
+        "--n_classes_known", type=int, default=45, help="Number of known classes"
+    )
+    parser.add_argument(
+        "--n_classes_tot", type=int, default=65, help="Number of unknown classes"
+    )
 
     # dataset path
-    parser.add_argument("--path_dataset", default="/.../AML-Project/data", help="Path where the Office-Home dataset is located")
+    parser.add_argument(
+        "--path_dataset",
+        default="./AML-Project/data",
+        help="Path where the Office-Home dataset is located",
+    )
 
     # data augmentation
-    parser.add_argument("--min_scale", default=0.8, type=float, help="Minimum scale percent")
-    parser.add_argument("--max_scale", default=1.0, type=float, help="Maximum scale percent")
+    parser.add_argument(
+        "--min_scale", default=0.8, type=float, help="Minimum scale percent"
+    )
+    parser.add_argument(
+        "--max_scale", default=1.0, type=float, help="Maximum scale percent"
+    )
     parser.add_argument("--jitter", default=0.4, type=float, help="Color jitter amount")
-    parser.add_argument("--random_grayscale", default=0.1, type=float,help="Randomly greyscale the image")
+    parser.add_argument(
+        "--random_grayscale",
+        default=0.1,
+        type=float,
+        help="Randomly greyscale the image",
+    )
 
     # training parameters
     parser.add_argument("--image_size", type=int, default=222, help="Image size")
     parser.add_argument("--batch_size", type=int, default=128, help="Batch size")
-    parser.add_argument("--learning_rate", type=float, default=0.001, help="Learning rate")
+    parser.add_argument(
+        "--learning_rate", type=float, default=0.001, help="Learning rate"
+    )
 
-    parser.add_argument("--epochs_step1", type=int, default=10, help="Number of epochs of step1 for known/unknown separation")
-    parser.add_argument("--epochs_step2", type=int, default=10, help="Number of epochs of step2 for source-target adaptation")
+    parser.add_argument(
+        "--epochs_step1",
+        type=int,
+        default=10,
+        help="Number of epochs of step1 for known/unknown separation",
+    )
+    parser.add_argument(
+        "--epochs_step2",
+        type=int,
+        default=10,
+        help="Number of epochs of step2 for source-target adaptation",
+    )
 
-    parser.add_argument("--train_all", type=bool, default=True, help="If true, all network weights will be trained")
+    parser.add_argument(
+        "--train_all",
+        type=bool,
+        default=True,
+        help="If true, all network weights will be trained",
+    )
 
-    parser.add_argument("--weight_RotTask_step1", type=float, default=0.5, help="Weight for the rotation loss in step1")
-    parser.add_argument("--weight_RotTask_step2", type=float, default=0.5, help="Weight for the rotation loss in step2")
-    parser.add_argument("--threshold", type=float, default=0.5, help="Threshold for the known/unkown separation")
+    parser.add_argument(
+        "--weight_RotTask_step1",
+        type=float,
+        default=0.5,
+        help="Weight for the rotation loss in step1",
+    )
+    parser.add_argument(
+        "--weight_RotTask_step2",
+        type=float,
+        default=0.5,
+        help="Weight for the rotation loss in step2",
+    )
+    parser.add_argument(
+        "--threshold",
+        type=float,
+        default=0.5,
+        help="Threshold for the known/unkown separation",
+    )
 
     # tensorboard logger
-    parser.add_argument("--tf_logger", type=bool, default=True, help="If true will save tensorboard compatible logs")
-    parser.add_argument("--folder_name", default=None, help="Used by the logger to save logs")
+    parser.add_argument(
+        "--tf_logger",
+        type=bool,
+        default=True,
+        help="If true will save tensorboard compatible logs",
+    )
+    parser.add_argument(
+        "--folder_name", default=None, help="Used by the logger to save logs"
+    )
 
     return parser.parse_args()
 
@@ -66,6 +124,7 @@ class Trainer:
     Class for the Trainer.
 
     """
+
     def __init__(self, args):
         """
         Initialisation of the trainer object
@@ -83,9 +142,9 @@ class Trainer:
         # ResNet model
         self.feature_extractor = resnet18_feat_extractor()
         # Initiate object classifier with input_size=512, and numbers of classes is known classes + 1 (the unknown class, trained only in step2)
-        self.obj_classifier = Classifier(512,self.args.n_classes_known+1)
+        self.obj_classifier = Classifier(512, self.args.n_classes_known + 1)
         # Initiate rotation classifier with input_size=512*2 and 4 classes: [0, 90, 180, 270]
-        self.rot_classifier = Classifier(512*2,4)
+        self.rot_classifier = Classifier(512 * 2, 4)
 
         # Load model into the device
         self.feature_extractor = self.feature_extractor.to(self.device)
@@ -93,20 +152,24 @@ class Trainer:
         self.rot_cls = self.rot_classifier.to(self.device)
 
         # Path to file with the list of known class images, source is the domain of the image (from command line argument, default is Art)
-        source_path_file = 'txt_list/'+args.source+'_known.txt'
+        source_path_file = "txt_list/" + args.source + "_known.txt"
         # Create the source data loader, providing an iterable over the dataset - using map-style dataset.
-        self.source_loader = data_helper.get_train_dataloader(args,source_path_file)
+        self.source_loader = data_helper.get_train_dataloader(args, source_path_file)
 
         # Path to the file with the list of target images (from command line argument, default is Clipart)
-        target_path_file = 'txt_list/' + args.target + '.txt'
+        target_path_file = "txt_list/" + args.target + ".txt"
         # Create two data loaders for the target dataset, one for training and one for evaluation.
         # (Probably this is a typo and the first one should be .get_train_dataloader as in step 2 below)
-        self.target_loader_train = data_helper.get_val_dataloader(args,target_path_file)
-        self.target_loader_eval = data_helper.get_val_dataloader(args,target_path_file)
+        self.target_loader_train = data_helper.get_val_dataloader(
+            args, target_path_file
+        )
+        self.target_loader_eval = data_helper.get_val_dataloader(args, target_path_file)
 
-        print("Source: ",self.args.source," Target: ",self.args.target)
-        print("Dataset size: source %d, target %d" % (len(self.source_loader.dataset), len(self.target_loader_train.dataset)))
-
+        print("Source: ", self.args.source, " Target: ", self.args.target)
+        print(
+            "Dataset size: source %d, target %d"
+            % (len(self.source_loader.dataset), len(self.target_loader_train.dataset))
+        )
 
     def do_training(self):
         """
@@ -115,27 +178,59 @@ class Trainer:
         """
 
         # Run step 1: train the object and rotation classifier
-        print('Step 1 --------------------------------------------')
-        step1(self.args,self.feature_extractor,self.rot_cls,self.obj_cls,self.source_loader,self.device)
+        print("Step 1 --------------------------------------------")
+        step1(
+            self.args,
+            self.feature_extractor,
+            self.rot_cls,
+            self.obj_cls,
+            self.source_loader,
+            self.device,
+        )
 
         # Evaluate the roation classifier on the target domain to split target into known and unknown classes.
-        print('Target - Evaluation -- for known/unknown separation')
+        print("Target - Evaluation -- for known/unknown separation")
         # Returns a random integer (don't really understand why - just to add to the generated file names?)
-        rand = evaluation(self.args,self.feature_extractor,self.rot_cls,self.target_loader_eval,self.device)
+        rand = evaluation(
+            self.args,
+            self.feature_extractor,
+            self.rot_cls,
+            self.target_loader_eval,
+            self.device,
+        )
 
         # Step 2 set up
         # Create a new data loader for the known source images
-        source_path_file = 'new_txt_list/' + self.args.source + '_known_'+str(rand)+'.txt'
-        self.source_loader = data_helper.get_train_dataloader(self.args,source_path_file)
+        source_path_file = (
+            "new_txt_list/" + self.args.source + "_known_" + str(rand) + ".txt"
+        )
+        self.source_loader = data_helper.get_train_dataloader(
+            self.args, source_path_file
+        )
 
-        #Create train and evaluation data loaders for the known target images
-        target_path_file = 'new_txt_list/' + self.args.target + '_known_' + str(rand) + '.txt'
-        self.target_loader_train = data_helper.get_train_dataloader(self.args,target_path_file)
-        self.target_loader_eval = data_helper.get_val_dataloader(self.args,target_path_file)
+        # Create train and evaluation data loaders for the known target images
+        target_path_file = (
+            "new_txt_list/" + self.args.target + "_known_" + str(rand) + ".txt"
+        )
+        self.target_loader_train = data_helper.get_train_dataloader(
+            self.args, target_path_file
+        )
+        self.target_loader_eval = data_helper.get_val_dataloader(
+            self.args, target_path_file
+        )
 
         # Run step 2: on known part do source-target domain adaptation, on unknown part of target train the unknown class
-        print('Step 2 --------------------------------------------')
-        step2(self.args,self.feature_extractor,self.rot_cls,self.obj_cls,self.source_loader,self.target_loader_train,self.target_loader_eval,self.device)
+        print("Step 2 --------------------------------------------")
+        step2(
+            self.args,
+            self.feature_extractor,
+            self.rot_cls,
+            self.obj_cls,
+            self.source_loader,
+            self.target_loader_train,
+            self.target_loader_eval,
+            self.device,
+        )
 
 
 def main():
@@ -144,10 +239,10 @@ def main():
     # Get arguments from the command line
     args = get_args()
 
-    # Create Trainer object 
+    # Create Trainer object
     trainer = Trainer(args)
 
-    #Initiate training
+    # Initiate training
     trainer.do_training()
 
 
@@ -156,5 +251,5 @@ if __name__ == "__main__":
     # Enable cudnn to benchmark multiple convolution algorithms and select the fastest.
     torch.backends.cudnn.benchmark = True
 
-    #Launch main program
+    # Launch main program
     main()
