ba59b6094ce8c51a9b77aa109a6a177cecfc4fec
Ehsan Ansari Nejad <s288903@studenti.polito.it>
2022-01-17 15:12:39 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,97 +1,171 @@
-import random
-from typing import List, Optional, Tuple
-
-import torch
 import torch.utils.data as data
-import torchvision.transforms as transforms
 from PIL import Image
+from random import random
+import random
+import torchvision
+import torchvision.transforms.functional as TF
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import List, Optional
+import torchvision.transforms as transforms
 
 
-def _dataset_info(txt_labels) -> Tuple[List[str], List[int]]:
-    """It will be used in data_helper to retrieve sample's name and associated label."""
-    with open(txt_labels, "r") as f:
+def _dataset_info(txt_labels):
+    with open(txt_labels, 'r') as f:
         images_list = f.readlines()
 
     file_names = []
     labels = []
     for row in images_list:
-        row = row.split(" ")
+        row = row.split(' ')
         file_names.append(row[0])
         labels.append(int(row[1]))
 
     return file_names, labels
 
 
-# It returns train_dataset
-class Dataset(data.Dataset):
+class JigSawDataset(data.Dataset):
+    def __init__(
+            self,
+            names: List[str],
+            labels: List[int],
+            path_dataset: str,
+            jig_classes=30, img_transformer: Optional[transforms.Compose] = None):
+        self.data_path = path_dataset
+        self.names = names
+        self.labels = labels
+        self.image_transformer = img_transformer
+
+        self.permutations = self.__retrieve_permutations(jig_classes)
+        self.grid_size = 3
+        self.image_resize = transforms.Compose([
+            transforms.Resize(256, Image.BILINEAR),
+            transforms.CenterCrop(255)])
+        self.augment_tile = transforms.Compose([
+            transforms.RandomCrop(64),
+            transforms.Resize((75, 75), Image.BILINEAR),
+            transforms.Lambda(rgb_jittering),
+            transforms.ToTensor()
+        ])
+
+    def get_tile(self, img, n):
+        w = float(img.size[0]) / self.grid_size
+        y = int(n / self.grid_size)
+        x = n % self.grid_size
+        tile = img.crop([x * w, y * w, (x + 1) * w, (y + 1) * w])
+        tile = self.augment_tile(tile)
+        return tile
+
+    def __getitem__(self, index):
+     # Get and process an image
+        image_path = self.data_path + "/" + self.names[index]
+        img = Image.open(image_path).convert("RGB")
+        if self.image_transformer:
+            img = self.image_transformer(img)
+
+        if img.size[0] != 255:
+            img = self.image_resize(img)
+
+        # 9 is number of grids
+        tiles = [None] * 9
+        for n in range(n_grids):
+            tiles[n] = self.get_tile(img, n)
+
+        index = random.randrange(len(self.permutations) + 1)
+        if index == 0:
+            data = tiles
+        else:
+            data = [tiles[self.permutations[order - 1][t]]
+                    for t in range(n_grids)]
+
+        data = torch.stack(data, 0)
+        return img, int(self.labels[index]), data, int(index)
+
+    def __len__(self):
+        return len(self.names)
+
+
+class JigsawTestDataset(data.Dataset):
     def __init__(
         self,
         names: List[str],
         labels: List[int],
         path_dataset: str,
-        img_transformer: Optional[transforms.Compose] = None,
+        jig_classes=30, img_transformer: Optional[transforms.Compose] = None,
     ):
         self.data_path = path_dataset
         self.names = names
         self.labels = labels
         self.image_transformer = img_transformer
 
-    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int, torch.Tensor, int]:
-        # Get and process image
+        self.permutations = self.__retrieve_permutations(jig_classes)
+        self.grid_size = 3
+        self.image_resize = transforms.Compose([
+            transforms.Resize(256, Image.BILINEAR),
+            transforms.CenterCrop(255)])
+        self.augment_tile = transforms.Compose([
+            transforms.RandomCrop(64),
+            transforms.Resize((75, 75), Image.BILINEAR),
+            transforms.Lambda(rgb_jittering),
+            transforms.ToTensor()
+        ])
+
+        def get_tile(self, img, n):
+            w = float(img.size[0]) / self.grid_size
+            y = int(n / self.grid_size)
+            x = n % self.grid_size
+            tile = img.crop([x * w, y * w, (x + 1) * w, (y + 1) * w])
+            tile = self.augment_tile(tile)
+        return tile
+
+    def __getitem__(self, index):
+        # Get and process an image
         image_path = self.data_path + "/" + self.names[index]
         img = Image.open(image_path).convert("RGB")
         if self.image_transformer:
             img = self.image_transformer(img)
-        # produce a random label between 0,1,2,3
-        index_rot = random.randrange(4)
-        # The angles of rotation are 0°, 90°, 180°, 270° that correspond to labels 0, 1, 2, 3.
-        if index_rot == 0:
-            img_rot = img
-        elif index_rot == 1:
-            img_rot = torch.rot90(img, k=1, dims=[1, 2])
-        elif index_rot == 2:
-            img_rot = torch.rot90(img, k=2, dims=[1, 2])
-        elif index_rot == 3:
-            img_rot = torch.rot90(img, k=3, dims=[1, 2])
 
-        return img, self.labels[index], img_rot, index_rot
+        if img.size[0] != 255:
+            img = self.image_resize(img)
+
+        # 9 is number of grids
+        tiles = [None] * 9
+        for n in range(n_grids):
+            tiles[n] = self.get_tile(img, n)
+
+        index = random.randrange(len(self.permutations) + 1)
+        if index == 0:
+            data = tiles
+        else:
+            data = [tiles[self.permutations[order - 1][t]]
+                    for t in range(n_grids)]
+
+        data = torch.stack(data, 0)
+
+        show([img, data])
+        return img, int(self.labels[index]), data, int(index)
 
     def __len__(self):
         return len(self.names)
 
 
-# It returns test_dataset
-class TestDataset(data.Dataset):
-    def __init__(
-        self,
-        names: List[str],
-        labels: List[int],
-        path_dataset: str,
-        img_transformer: Optional[transforms.Compose] = None,
-    ):
-        self.data_path = path_dataset
-        self.names = names
-        self.labels = labels
-        self.image_transformer = img_transformer
+# sphinx_gallery_thumbnail_path = "../../gallery/assets/visualization_utils_thumbnail.png"
 
-    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int, torch.Tensor, int]:
-        image_path = self.data_path + "/" + self.names[index]
-        img = Image.open(image_path).convert("RGB")
-        if self.image_transformer:
-            img = self.image_transformer(img)
-        # produce a random label between 0,1,2,3
-        index_rot = random.randrange(4)
-        # The angles of rotation are 0°, 90°, 180°, 270° that correspond to labels 0, 1, 2, 3.
-        if index_rot == 0:
-            img_rot = img
-        elif index_rot == 1:
-            img_rot = torch.rot90(img, k=1, dims=[1, 2])
-        elif index_rot == 2:
-            img_rot = torch.rot90(img, k=2, dims=[1, 2])
-        elif index_rot == 3:
-            img_rot = torch.rot90(img, k=3, dims=[1, 2])
 
-        return img, self.labels[index], img_rot, index_rot
+#plt.rcParams["savefig.bbox"] = 'tight'
 
-    def __len__(self):
-        return len(self.names)
+
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+        # write image to file
+        plt.savefig(f"figs/{random.randint(10000) + i}.png")
+        plt.cla()
+    plt.close(fix)
